82258953ae9c11673b207288b2ef9db57027ec3e
askaromorbekov <omorbekov.a@yandex.ru>
2019-03-28 11:40:29 +0600

    1.3 JavaDoc. Code convention [@124776]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,7 +1,17 @@
+﻿/**
+ * Класс Calculate для вывода строки
+ * @author Askar Omorbekov (omorbekov.a@yandex.ru)
+ * @since 28.03.2019
+ * @version 0.1
+*/
 package ru.job4j.calculate;
 
 public class Calculate{
-	
+	/**
+	 * Main
+	 * Конструктор для вывода строки в консоль
+	 * @param args - args
+	*/
 	public static void main(String[] args){
 		System.out.println("Hello World");
 	}
---
+++ chapter_001/src/main/java/ru/job4j/calculate/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Класс Calculate для вывода строки
+ *
+ * @author Askar Omorbekov (omorbekov.a@yandex.ru)
+ * @version 0.1
+ * @since 28.03.2019
+*/
+package ru.job4j.calculate;
