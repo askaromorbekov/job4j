a3151473eb6870e1e2152ef5ac1c721282905d12
askaromorbekov <omorbekov.a@yandex.ru>
2019-03-31 18:25:34 +0600

    3.4. Расстояние между точками в системе координат[#124767]

---
+++ chapter_001/src/main/java/ru/job4j/condition/Point.java
@@ -0,0 +1,21 @@
+package ru.job4j.condition;
+
+/**
+ * Расстояние между точками в системе координат
+ */
+public class Point {
+    /**
+     * Расчет растояния между точками в системе координат
+     * @param x1 точка в системе координат.
+     * @param y1 точка в системе координат.
+     * @param x2 точка в системе координат.
+     * @param y2 точка в системе координат.
+     * @return корень sqrt.
+     */
+    public double distance(int x1, int y1, int x2, int y2) {
+        double first = Math.pow(x2 - x1, 2);
+        double two = Math.pow(y2 - y1, 2);
+        double sqrt = Math.sqrt(first + two);
+        return sqrt;
+    }
+}
---
+++ chapter_001/src/test/java/ru/job4j/condition/PointTest.java
@@ -0,0 +1,26 @@
+package ru.job4j.condition;
+
+import org.junit.Test;
+import static org.hamcrest.Matchers.closeTo;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Test Расстояние между точками
+ *
+ * @author Askar Omorbekov (omorbekov.a@yandex.ru)
+ * @version 0.5
+ * @since 31.03.2019
+ */
+
+public class PointTest {
+    /**
+     * Тест Point()
+     */
+    @Test
+    public void whenZeroAndTenThenTen() {
+        Point point = new Point();
+        double result = point.distance(5, 4, 10, 8);
+        assertThat(result, closeTo(6D, 0.5));
+    }
+
+}
