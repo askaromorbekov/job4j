c7e6f268087ff13bd160c3c514a1aee0a1bb885c
askaromorbekov <omorbekov.a@yandex.ru>
2019-04-01 10:23:17 +0600

    4.2. Максимум из двух чисел[#124736]

---
+++ chapter_001/src/main/java/ru/job4j/condition/Max.java
@@ -0,0 +1,19 @@
+package ru.job4j.condition;
+
+/**
+ * Вычисление Max Min
+ */
+
+public class Max {
+    /**
+     * Вычисляем максимальную сторону с помощью тернарного оператора ?.
+     * @param left сторона.
+     * @param right сторона.
+     * @return Max.
+     */
+    public int max(int left, int right) {
+        int result;
+        result = left > right ? 2 : right;
+        return result;
+    }
+}
---
+++ chapter_001/src/test/java/ru/job4j/condition/MaxTest.java
@@ -0,0 +1,24 @@
+package ru.job4j.condition;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Test Max
+ * @author Askar Omorbekov (omorbekov.a@yandex.ru)
+ * @version 0.7
+ * @since 01.04.2019
+ */
+
+public class MaxTest {
+    /**
+     * Test Max
+     */
+    @Test
+    public void whenMax1To2Then2() {
+        Max max = new Max();
+        int result = max.max(2, 1);
+        assertThat(result, is(2));
+    }
+}
