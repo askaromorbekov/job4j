01f57f2c2a16e003dd79253424e3879dd34c16b9
askaromorbekov <omorbekov.a@yandex.ru>
2019-04-03 00:27:57 +0600

    4.1. Глупый бот.[#124733]

--- chapter_001/src/main/java/ru/job4j/condition/DummyBot.java
+++ chapter_001/src/main/java/ru/job4j/condition/DummyBot.java
@@ -12,11 +12,13 @@ public class DummyBot {
      * @return Ответ бота.
      */
     public String answer(String question) {
-        String rsl = "Это ставит меня в тупик. Задайте другой вопрос.";
+        String rsl;
         if ("Привет, Бот.".equals(question)) {
             rsl = "Привет, умник.";
-        } else if ("Пока".equals(question)) {
+        } else if ("Пока.".equals(question)) {
             rsl = "До скорой встречи.";
+        } else {
+            rsl = "Это ставит меня в тупик. Задайте другой вопрос.";
         }
         return rsl;
     }
